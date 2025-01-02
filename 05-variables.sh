7d0ceacdad78f2191d5c628ea98cac273e029517
shalinic26 <shalinichintala@gmail.com>
2025-01-03 00:57:37 +0530

    Run time username and hiding password given

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,7 @@ echo "Please enter your name::" # text entered here will be added as value to va
 read -s USERNAME # Here USERNAME is the variable name;;; # Here -s command is for hiding the username 
 
 echo "username entered: $USERNAME"
+
+echo "Please enter your password::"
+
+read -s PASSWORD # We are not printing password 
