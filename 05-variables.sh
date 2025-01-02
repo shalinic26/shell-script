f22a72b8168716ecc213b213a0f9e40d255766df
shalinic26 <shalinichintala@gmail.com>
2025-01-03 00:54:04 +0530

    Hiding run time username given

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,6 @@
 
 echo "Please enter your name::" # text entered here will be added as value to variable
 
-read USERNAME # Here USERNAME is the variable name
+read -s USERNAME # Here USERNAME is the variable name;;; # Here -s command is for hiding the username 
 
 echo "username entered: $USERNAME"
