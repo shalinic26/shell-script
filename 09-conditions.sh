cfd8b7d4863ba086dcf40bf548eec5fe7589e9c8
shalinic26 <shalinichintala@gmail.com>
2025-01-03 18:15:06 +0530

    My OWN Conditions script added

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-NUMBER=$1
+#NUMBER=$1
 
-echo "Entered number is: $NUMBER"
+#echo "Entered number is: $NUMBER"
+
+#if [ $NUMBER -gt 100 ]
+#then
+ #   echo "Given number is greater than 100"
+#else
+ #   echo "Given number is less than or equal to 100"
+#fi
+
+echo "Enter a number:"
+
+read -s NUMBER
+
+echo "Number entered is: $NUMBER"
 
 if [ $NUMBER -gt 100 ]
 then
-    echo "Given number is greater than 100"
+    echo "Number is greater than 100"
 else
-    echo "Given number is less than or equal to 100"
+    echo "Number is less than or equal to 100"
 fi
-
-
 
