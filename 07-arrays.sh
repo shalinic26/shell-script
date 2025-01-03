caea2fc782c50f02dc24b23e96f655aa00abee29
shalinic26 <shalinichintala@gmail.com>
2025-01-03 16:48:30 +0530

    Modified Movies array list script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -4,9 +4,9 @@ MOVIES=("Mahanati" "Eega" "Baahubali")
 
 echo "First movie is: ${MOVIES[0]}"
 
-echo "Second movie is: ${MOVIES[0]}"
+echo "Second movie is: ${MOVIES[1]}"
 
-echo "Third movie is: ${MOVIES[0]}"
+echo "Third movie is: ${MOVIES[2]}"
 
 # If we want the list of all movies to be printed on console then we need to give @
 
