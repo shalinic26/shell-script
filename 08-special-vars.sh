22b80333855927d12b483c705666661223c91ab5
shalinic26 <shalinichintala@gmail.com>
2025-01-03 17:10:00 +0530

    Modified Special Variables script for PWD

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -3,7 +3,7 @@
 echo "All Variables passed: $@"
 echo "Number of variables: $#"
 echo "Script name is: $0"
-echo "Present working directory: $pwd"
+echo "Present working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "Which user is running the script: $USER"
 echo "Process ID of current script: $$"
