8ae1c2124aee001b9570c95c5ee4c69614491d7e
shalinic26 <shalinichintala@gmail.com>
2025-01-06 15:45:03 +0530

    Logs backup script added

---
+++ 19-Logs-backup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # if user is not providing number of days, we are taking 14 as default
+
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){                 # This function needs previous command's (dnf install mysql -y) exit status as Input
+    if [ $1 -ne 0 ]             # -> $1 will have exit status of previous command i.e. dnf install mysql -y is previous command
+    then
+        echo -e "$2.....$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2......$G SUCCESS $N"
+    fi
+}
+
+mkdir -p $LOGS_FOLDER
+VALIDATE $? "Creating shell script logs directory"
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+USAGE(){
+    echo -e "$R USAGE:: $N sh 19-Logs-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(optional)>"
+    exit 1
+}
+
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
