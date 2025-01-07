8f084e47e0a147fc3af230466ad5b737b593e5dd
shalinic26 <shalinichintala@gmail.com>
2025-01-08 01:39:15 +0530

    disk usage script excluding % symbol

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -5,7 +5,8 @@ DISK_THRESHOLD=5 # In real projects it will be for monitoring 70% or 80%
 
 while read -r line
 do
-    USAGE=$(echo $line | awk -F " " '{print $6F}') # Here variable USAGE is related to 6th column name Use% which we are tryign to read
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1) # Here variable USAGE is related to 6th column name Use% which we are tryign to read
+    #In above line "cut" command is used for cutting % shown against every percentage so that we can compare the usage % number is <5 (ignore) or >5(mail/monitor) 
     #PARTITION=$(echo $line | awk -F " " '{print $7F}') -- Siva script
     #echo "Partition: $PARTITION, Usage: $USAGE" --- siva script
     MOUNTEDON=$(echo $line | awk -F " " '{print $7F}')
