327554d8bc0f3f904e67979317e37c588b5e95ff
shalinic26 <shalinichintala@gmail.com>
2025-01-08 01:56:26 +0530

    disk usage script comparision with threshold

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -2,6 +2,8 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5 # In real projects it will be for monitoring 70% or 80%
+MSG="" # Dont know why we need to write this ???
+
 
 while read -r line
 do
@@ -9,6 +11,14 @@ do
     #In above line "cut" command is used for cutting % shown against every percentage so that we can compare the usage % number is <5 (ignore) or >5(mail/monitor) 
     #PARTITION=$(echo $line | awk -F " " '{print $7F}') -- Siva script
     #echo "Partition: $PARTITION, Usage: $USAGE" --- siva script
-    MOUNTEDON=$(echo $line | awk -F " " '{print $7F}')
-    echo "MountedOn: $MOUNTEDON, Usage: $USAGE"
+    MOUNT_FOLDER=$(echo $line | awk -F " " '{print $7F}')
+    echo "MountFolder: $MOUNT_FOLDER, Usage: $USAGE"
+
+    # For comparing something we always need to write condition
+    if [ $USAGE -ge $DISK_THRESHOLD] #Here -ge means greater than or equal to     
+    then
+        MSG+="High Disk usage on mount folder: $MOUNT_FOLDER Usage is: $USAGE \n "
+    fi
 done <<< $DISK_USAGE
+
+echo -e "Message: $MSG" # -e means --> enabling 
